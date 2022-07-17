e0bd439fb9f97b11d8d4e2be878c389fd8339b90
Yuri Alves <yuripiresalves@gmail.com>
2022-07-16 22:20:31 -0300

    finish web

---
+++ desafios/01-c6-bank/js/scripts/main.js
@@ -0,0 +1,36 @@
+const header = document.getElementById('js-header');
+
+function fixedMenu() {
+  if (window.scrollY > 80) {
+    header.classList.add('fixed-menu');
+  } else {
+    header.classList.remove('fixed-menu');
+  }
+}
+
+document.addEventListener('scroll', fixedMenu);
+
+const allFilters = document.querySelectorAll('.js-btns button');
+const tabPane = document.querySelectorAll('.js-bottom');
+console.log(allFilters, tabPane);
+
+allFilters.forEach((filter, index) => {
+  filter.addEventListener('click', () => {
+    allFilters.forEach((item) => {
+      item.classList.remove('active');
+    });
+
+    tabPane.forEach((item) => {
+      item.classList.remove('active');
+    });
+
+    tabPane[index].classList.add('active');
+
+    filter.classList.add('active');
+  });
+});
+
+AOS.init({
+  duration: 800,
+  once: true,
+});
